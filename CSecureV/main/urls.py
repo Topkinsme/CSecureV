b2fa3d0040385ec18a00a7fe4deb38da08dd3e96
Saravanan <saravananr0104@gmail.com>
2025-03-31 19:23:16 +0530

    added pages

--- CSecureV/main/urls.py
+++ CSecureV/main/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('login',views.login,name='login'),
+    path('sign-up',views.sign_up,name='sign_up'),
+    path('main-page',views.main_page,name='main_page'),
+    path('profile',views.profile,name='profile'),
 ]
--- CSecureV/main/views.py
+++ CSecureV/main/views.py
@@ -1,4 +1,17 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("Hello, Django!")
+    return render(request, 'main/landing_page.html')
+
+def login(request):
+    return render(request,'main/login.html')
+
+def sign_up(request):
+    return render(request,'main/signup.html')
+
+def main_page(request):
+    return render(request,'main/main_page.html')
+
+def profile(request):
+    return render(request,'main/profile.html')
